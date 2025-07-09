0c3863222d562323c147ae06b7049b1d1a02f417
evanp <pikscher7023@students.d211.org>
2025-07-08 19:50:42 -0500

    Created sidebar

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const hamburger = document.querySelector("#toggle-btn");
+const sidebar = document.querySelector("#sidebar");
+const toggleBtn = document.querySelector("#toggle-btn");
 
-hamburger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
+if (document.documentElement.classList.contains("sidebar-expanded")) {
+  sidebar.classList.add("expand");
+}
+
+toggleBtn?.addEventListener("click", () => {
+  sidebar.classList.toggle("expand");
+
+  const isExpanded = sidebar.classList.contains("expand");
+  if (isExpanded) {
+    document.documentElement.classList.add("sidebar-expanded");
+    localStorage.setItem("sidebarState", "expanded");
+  } else {
+    document.documentElement.classList.remove("sidebar-expanded");
+    localStorage.setItem("sidebarState", "collapsed");
+  }
 });
